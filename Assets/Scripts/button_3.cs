401cb93386b1af19e72df99e1ce5d3b90e6d0c37
Henry <Henrymarsden@live.com>
2026-03-27 01:40:34 -0400

    BiggestB

--- Assets/Scripts/button_3.cs
+++ Assets/Scripts/button_3.cs
@@ -4,9 +4,17 @@ using System.Collections;
 public class button_3:MonoBehaviour
 {
     public player_movement player_movement_script;
-    
+    public loading_manager loading_manager_script;
+    public string game_name;
+
     public void play()
     {
-        StartCoroutine(player_movement_script.move_and_fade_out());
+        StartCoroutine(scene_swap_prep());
+    }
+
+    public IEnumerator scene_swap_prep()
+    {
+        yield return StartCoroutine(player_movement_script.move_and_fade_out());
+        StartCoroutine(loading_manager_script.start_loading(game_name));
     }
 }
